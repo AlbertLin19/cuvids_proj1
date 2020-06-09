f14fd8e145b9f99611a092cd5760b637286de44f
Albert Lin <Albertkuilin@gmail.com>
2020-06-09 15:45:33 -0400

    basic environment and django starting project created

---
+++ proj1_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Albert_Lin_2023\Desktop\GitHub Desktop Local Repos\CUvids 2020 Summer Internship\cuvids_proj1\proj1_env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
